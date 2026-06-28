fbca5bb71cf5b12e798cc7b24c4c74694a05779c
spycodger12 <73761958+spycodger12@users.noreply.github.com>
2026-06-28 19:37:50 +0200

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+# include <string.h>
+
+
+
+int main() {
+    char userInput[100];
+
+    printf("Type something\n"); // ask prompt
+    printf("---------------\n");
+
+    scanf("%99s", userInput);
+
+    if (strcmp(userInput, "hello") == 0) {
+        printf("Hello!\n");
+    } else {
+        printf("You wrote: %s\n", userInput);
+    }
+
+//    printf("You wrote: %s\n", userInput);
+    return 0;
+}
+
+// variables are like this: int NAME = value;
+// this is a comment
