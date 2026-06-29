d838621f6f9ac7a813bace767c5f277af9d9f282
Spycodger12 <samueldoctorescastroviejo@gmail.com>
2026-06-30 00:59:41 +0200

    Added folder with window for win. 8.1

---
+++ window-making/windows 8 test/main.c
@@ -0,0 +1,72 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+
+// 01-  window protocol
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+// 02-  universal entry for MinGW
+int main(int argc, char** argv) {
+    // get identifier from running instance
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    
+    // TEXT() for unicode compat.
+    const wchar_t CLASS_NAME[] = L"MyWindowClass";
+    
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszClassName = CLASS_NAME;
+
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, L"Error registering window class", L"Error", MB_ICONERROR);
+        return 0;
+    }
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"Compatible window 8.1/10/11",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
